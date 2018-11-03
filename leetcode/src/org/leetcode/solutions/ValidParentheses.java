1dc791fb8414aa1afb67c616fbf167f6b4f72c8e
zyc in home <271933294@qq.com>
2018-11-03 22:44:11 +0800

    add valid parentheses

---
+++ leetcode/src/org/leetcode/solutions/ValidParentheses.java
@@ -0,0 +1,24 @@
+package org.leetcode.solutions;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+	public boolean isValid(String s) {
+		Stack<Character> stack = new Stack<Character>();
+		for (char c : s.toCharArray()) {
+			if (c == '(')
+				stack.push(')');
+			else if (c == '{')
+				stack.push('}');
+			else if (c == '[')
+				stack.push(']');
+			else if (stack.isEmpty() || stack.pop() != c)
+				return false;
+		}
+		return stack.isEmpty();
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new ValidParentheses().isValid("({])"));
+	}
+}
