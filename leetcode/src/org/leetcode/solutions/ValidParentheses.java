e169d4e80c6adacc3bbc7a77736500d88b667e27
zyc in home <271933294@qq.com>
2018-11-03 22:51:15 +0800

    add explaination

--- leetcode/src/org/leetcode/solutions/ValidParentheses.java
+++ leetcode/src/org/leetcode/solutions/ValidParentheses.java
@@ -5,6 +5,9 @@ import java.util.Stack;
 public class ValidParentheses {
 	public boolean isValid(String s) {
 		Stack<Character> stack = new Stack<Character>();
+		// when the character is ')' or '}' or ']'
+		// only way to match left symbol is that the previous is '(' or '{' or '['
+		// so it's just like stack, last in first out
 		for (char c : s.toCharArray()) {
 			if (c == '(')
 				stack.push(')');
