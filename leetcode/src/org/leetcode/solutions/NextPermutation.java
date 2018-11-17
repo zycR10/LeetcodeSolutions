52575cfcd27aeaa94a18683ad1f732b45e84bcae
zuoyichen <271933294@qq.com>
2018-11-17 23:37:32 +0800

    modify next permutation, still wrong

--- leetcode/src/org/leetcode/solutions/NextPermutation.java
+++ leetcode/src/org/leetcode/solutions/NextPermutation.java
@@ -1,5 +1,7 @@
 package org.leetcode.solutions;
 
+import java.util.Arrays;
+
 public class NextPermutation {
 	public void nextPermutation(int[] nums) {
 		if (nums == null || nums.length == 0) {
@@ -12,8 +14,28 @@ public class NextPermutation {
 				break;
 			}
 		}
-		if(isExist) {
-			// TODO
+		if (isExist) {
+			generateNextPermutation(nums);
+		} else {
+			reverseArrays(nums);
+		}
+	}
+
+	private void reverseArrays(int[] nums) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void generateNextPermutation(int[] nums) {
+		// TODO Auto-generated method stub
+		int max = -1;
+		for (int i = nums.length - 1; i > 0; i++) {
+			if (nums[i] < max) {
+				Arrays.sort(nums, i + 1, nums.length - 1);
+			}
+			if (nums[i] > max) {
+				max = nums[i];
+			}
 		}
 	}
 }
