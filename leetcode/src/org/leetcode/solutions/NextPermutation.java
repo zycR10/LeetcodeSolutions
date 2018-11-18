078d696ba541cc6b42646f7feea39efe3d5e28f6
zuoyichen <271933294@qq.com>
2018-11-18 16:57:30 +0800

    optimize next permutation

--- leetcode/src/org/leetcode/solutions/NextPermutation.java
+++ leetcode/src/org/leetcode/solutions/NextPermutation.java
@@ -17,7 +17,8 @@ public class NextPermutation {
 			if (nums[i] < max) {
 				int ex = -1;
 				for (int j = i + 1; j < nums.length; j++) {
-					// find the smallest number of the numbers greater than nums[i]
+					// find the smallest number of the numbers greater than
+					// nums[i]
 					if (nums[j] > nums[i]) {
 						ex = j;
 						continue;
@@ -26,9 +27,11 @@ public class NextPermutation {
 				}
 				if (ex != -1) {
 					// exchange i and j
-					int temp = nums[i];
-					nums[i] = nums[ex];
-					nums[ex] = temp;
+					// int temp = nums[i];
+					// nums[i] = nums[ex];
+					// nums[ex] = temp;
+					swap(nums, i, ex);
+
 					// smallest number means order by ascending
 					Arrays.sort(nums, i + 1, nums.length);
 					return;
@@ -36,7 +39,26 @@ public class NextPermutation {
 			}
 			max = nums[i];
 		}
-		Arrays.sort(nums);
+		
+		// replace sort by reverse, more efficient.
+		// Arrays.sort(nums);
+		reverse(nums);
+	}
+
+	private void reverse(int[] nums) {
+		int i = 0;
+		int j = nums.length - 1;
+		while (i < j) {
+			swap(nums, i, j);
+			i++;
+			j--;
+		}
+	}
+
+	private void swap(int[] nums, int i, int j) {
+		int temp = nums[i];
+		nums[i] = nums[j];
+		nums[j] = temp;
 	}
 
 	public static void main(String arg[]) {
