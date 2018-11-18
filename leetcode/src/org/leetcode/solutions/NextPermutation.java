d65f3e41da064bcd4f92c5de8e1c4387b2a1e7f9
zuoyichen <271933294@qq.com>
2018-11-18 16:51:46 +0800

    complete problem

--- leetcode/src/org/leetcode/solutions/NextPermutation.java
+++ leetcode/src/org/leetcode/solutions/NextPermutation.java
@@ -7,35 +7,41 @@ public class NextPermutation {
 		if (nums == null || nums.length == 0) {
 			return;
 		}
-		boolean isExist = false;
-		for (int i = 1; i < nums.length; i++) {
-			if (nums[i] > nums[i - 1]) {
-				isExist = true;
-				break;
-			}
-		}
-		if (isExist) {
-			generateNextPermutation(nums);
-		} else {
-			reverseArrays(nums);
-		}
-	}
-
-	private void reverseArrays(int[] nums) {
-		// TODO Auto-generated method stub
-
+		generateNextPermutation(nums);
 	}
 
 	private void generateNextPermutation(int[] nums) {
-		// TODO Auto-generated method stub
 		int max = -1;
-		for (int i = nums.length - 1; i > 0; i++) {
+		for (int i = nums.length - 1; i >= 0; i--) {
+			// if nums order by desc, it doesn't has next permutation
 			if (nums[i] < max) {
-				Arrays.sort(nums, i + 1, nums.length - 1);
+				int ex = -1;
+				for (int j = i + 1; j < nums.length; j++) {
+					// find the smallest number of the numbers greater than nums[i]
+					if (nums[j] > nums[i]) {
+						ex = j;
+						continue;
+					}
+					break;
+				}
+				if (ex != -1) {
+					// exchange i and j
+					int temp = nums[i];
+					nums[i] = nums[ex];
+					nums[ex] = temp;
+					// smallest number means order by ascending
+					Arrays.sort(nums, i + 1, nums.length);
+					return;
+				}
 			}
-			if (nums[i] > max) {
-				max = nums[i];
-			}
+			max = nums[i];
 		}
+		Arrays.sort(nums);
+	}
+
+	public static void main(String arg[]) {
+		int[] nums = { 1, 3, 2 };
+		new NextPermutation().nextPermutation(nums);
+		System.out.println(nums);
 	}
 }
