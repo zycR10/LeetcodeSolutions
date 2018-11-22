b945230c59531fcb533fdbe9f64bc4d3e80d9944
zuoyichen <zuoyichen@sunlands.com>
2018-11-22 15:30:22 +0800

    add longest valid parentheses

---
+++ leetcode/src/org/leetcode/solutions/LongestValidParentheses.java
@@ -0,0 +1,24 @@
+package org.leetcode.solutions;
+
+import java.util.Stack;
+
+public class LongestValidParentheses {
+    private int longestValidParentheses(String s) {
+        int max = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.empty()) {
+                    stack.push(i);
+                } else {
+                    max = Math.max(max, i - stack.peek());
+                }
+            }
+        }
+        return max;
+    }
+}
