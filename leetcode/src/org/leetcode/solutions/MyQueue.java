48231bb605d0fd0fe2500f867fb8e5d5179dd6e6
zyc_hp_laptop <271933294@qq.com>
2018-11-26 16:59:05 +0800

    test for new laptop environment

--- leetcode/src/org/leetcode/solutions/MyQueue.java
+++ leetcode/src/org/leetcode/solutions/MyQueue.java
@@ -6,18 +6,18 @@ public class MyQueue {
 	private Stack<Integer> in;
 	private Stack<Integer> out;
 
-	/** Initialize your data structure here. */
+	// Initialize your data structure here.
 	public MyQueue() {
 		in = new Stack<>();
 		out = new Stack<>();
 	}
 
-	/** Push element x to the back of queue. */
+	// Push element x to the back of queue.
 	public void push(int x) {
 		in.push(x);
 	}
 
-	/** Removes the element from in front of queue and returns that element. */
+	// Removes the element from in front of queue and returns that element.
 	public int pop() {
 		if (out.isEmpty()) {
 			while (!in.isEmpty()) {
