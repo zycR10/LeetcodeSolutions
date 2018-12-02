6345e6e54e5fadb953aba32850d53e4c28f1a190
zuoyichen <271933294@qq.com>
2018-12-02 22:48:28 +0800

    add valid sudoku solution v1.0

--- leetcode/src/org/leetcode/solutions/ValidSudoku.java
+++ leetcode/src/org/leetcode/solutions/ValidSudoku.java
@@ -1,4 +1,25 @@
 package org.leetcode.solutions;
 
 public class ValidSudoku {
+    public boolean isValidSudoku(char[][] board) {
+        for (int i = 0, j = 0; i < board.length && j < board[0].length; i++, j++) {
+            if (i % 3 == 0) {
+                if (!validateGird(board)) {
+                    return false;
+                }
+            }
+            if (!validateRowAndColumn(board)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean validateRowAndColumn(char[][] board) {
+        return true;
+    }
+
+    private boolean validateGird(char[][] board) {
+        return true;
+    }
 }
