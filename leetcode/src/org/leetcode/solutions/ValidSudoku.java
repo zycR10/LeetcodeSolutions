34f62afa1fa7b5b1e7de75482aa71f28b85bd08e
zuoyichen <271933294@qq.com>
2018-12-02 23:42:23 +0800

    add valid row & column v2.0

--- leetcode/src/org/leetcode/solutions/ValidSudoku.java
+++ leetcode/src/org/leetcode/solutions/ValidSudoku.java
@@ -2,24 +2,41 @@ package org.leetcode.solutions;
 
 public class ValidSudoku {
     public boolean isValidSudoku(char[][] board) {
-        for (int i = 0, j = 0; i < board.length && j < board[0].length; i++, j++) {
+        for (int i = 0; i < board.length; i++) {
             if (i % 3 == 0) {
                 if (!validateGird(board)) {
                     return false;
                 }
             }
-            if (!validateRowAndColumn(board)) {
+            if (!validateRowAndColumn(board, i)) {
                 return false;
             }
         }
         return true;
     }
 
-    private boolean validateRowAndColumn(char[][] board) {
+    private boolean validateRowAndColumn(char[][] board, int n) {
+        char[] row = board[n];
+        char[] valid = new char[9];
+        int index = 0;
+        for(int i = 0;i < row.length; i++) {
+            index = row[i] - '1';
+            if(Character.isDigit(valid[index])) {
+                return false;
+            }else {
+                valid[index] = row[i];
+            }
+        }
         return true;
     }
 
     private boolean validateGird(char[][] board) {
         return true;
+    }
+
+    public static void main(String[] args) {
+        char[] c = new char[10];
+        c[3] = '8';
+        System.out.println(Character.isDigit(c[3]));
     }
 }
