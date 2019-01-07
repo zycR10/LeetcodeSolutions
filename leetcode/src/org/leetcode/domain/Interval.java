6e7c5306210e8dc0517f5d545eb3b73b3f85145f
zuoyichen@sunlands.com <zuoyichen@sunlands.com>
2019-01-07 11:37:27 +0800

    add solution for merge intervals

--- leetcode/src/org/leetcode/domain/Interval.java
+++ leetcode/src/org/leetcode/domain/Interval.java
@@ -8,12 +8,12 @@ public class Interval {
     public int start;
     public int end;
 
-    Interval() {
+    public Interval() {
         start = 0;
         end = 0;
     }
 
-    Interval(int s, int e) {
+    public Interval(int s, int e) {
         start = s;
         end = e;
     }
--- leetcode/src/org/leetcode/solutions/MergeIntervals.java
+++ leetcode/src/org/leetcode/solutions/MergeIntervals.java
@@ -2,42 +2,65 @@ package org.leetcode.solutions;
 
 import org.leetcode.domain.Interval;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @Author: Zuo Yichen
  * @Date: 2019/1/4 16:17
  */
 public class MergeIntervals {
+
+    // myself solution, beats 60%
+//    public List<Interval> merge(List<Interval> intervals) {
+//        if (intervals == null || intervals.size() == 0) {
+//            return new ArrayList<>();
+//        }
+//        List<Interval> res = new LinkedList<>();
+//        // sort by start
+//        intervals.sort(new Comparator<Interval>() {
+//            @Override
+//            public int compare(Interval o1, Interval o2) {
+//                if(o1.start > o2.start) {
+//                    return 1;
+//                }
+//                if(o1.start < o2.start) {
+//                    return -1;
+//                }
+//                return 0;
+//            }
+//        });
+//        res.add(intervals.get(0));
+//        // check if current and previous interval have overlapped part.
+//        for (int i = 1; i < intervals.size(); i++) {
+//            Interval interval = intervals.get(i);
+//            Interval preInterval = res.get(res.size() - 1);
+//            if (interval.start > preInterval.end) {
+//                res.add(interval);
+//            } else {
+//                preInterval.end = Math.max(interval.end, preInterval.end);
+//            }
+//        }
+//        return res;
+//    }
+
     public List<Interval> merge(List<Interval> intervals) {
-        if (intervals == null || intervals.size() == 0) {
-            return new ArrayList<>();
-        }
         List<Interval> res = new LinkedList<>();
-        intervals.sort(new Comparator<Interval>() {
-            @Override
-            public int compare(Interval o1, Interval o2) {
-                if(o1.start > o2.start) {
-                    return 1;
-                }
-                if(o1.start < o2.start) {
-                    return -1;
-                }
-                return 0;
+        int[] start = new int[intervals.size()];
+        int[] end = new int[intervals.size()];
+        for (int i = 0; i < intervals.size(); i++) {
+            start[i] = intervals.get(i).start;
+            end[i] = intervals.get(i).end;
+        }
+        Arrays.sort(start);
+        Arrays.sort(end);
+
+        for (int i = 0, j = 0; i < intervals.size(); ) {
+            while (j + 1 < intervals.size() && start[j + 1] <= end[j]) {
+                j++;
             }
-        });
-        res.add(intervals.get(0));
-        for (int i = 1; i < intervals.size(); i++) {
-            Interval interval = intervals.get(i);
-            Interval preInterval = res.get(res.size() - 1);
-            if (interval.start > preInterval.end) {
-                res.add(interval);
-            } else {
-                preInterval.end = Math.max(interval.end, preInterval.end);
-            }
+            res.add(new Interval(start[i], end[j]));
+            i = j + 1;
+            j++;
         }
         return res;
     }
