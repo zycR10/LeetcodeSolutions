7a019dfd57d38335331340c0730e2082d376b149
zuoyichen@sunlands.com <zuoyichen@sunlands.com>
2019-01-17 20:14:57 +0800

    add minimum path sum

---
+++ leetcode/src/org/leetcode/solutions/MinimumPathSum.java
@@ -0,0 +1,26 @@
+package org.leetcode.solutions;
+
+/**
+ * Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+ * <p>
+ * Note: You can only move either down or right at any point in time.
+ * <p>
+ * Example:
+ * <p>
+ * Input:
+ * [
+ * [1,3,1],
+ * [1,5,1],
+ * [4,2,1]
+ * ]
+ * Output: 7
+ * Explanation: Because the path 1→3→1→1→1 minimizes the sum.
+ *
+ * @Author: Zuo Yichen
+ * @Date: 2019/1/17 17:28
+ */
+public class MinimumPathSum {
+    public int minPathSum(int[][] grid) {
+
+    }
+}
