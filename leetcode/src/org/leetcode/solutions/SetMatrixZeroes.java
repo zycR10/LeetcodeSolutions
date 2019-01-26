d8c57946d22c0ef4c6e0cff019a9312170fe7b92
zuoyichen@sunlands.com <zuoyichen@sunlands.com>
2019-01-26 13:52:55 +0800

    add set matrix zeroes

---
+++ leetcode/src/org/leetcode/solutions/SetMatrixZeroes.java
@@ -0,0 +1,102 @@
+package org.leetcode.solutions;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in-place.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * [
+ * [1,1,1],
+ * [1,0,1],
+ * [1,1,1]
+ * ]
+ * Output:
+ * [
+ * [1,0,1],
+ * [0,0,0],
+ * [1,0,1]
+ * ]
+ * Example 2:
+ * <p>
+ * Input:
+ * [
+ * [0,1,2,0],
+ * [3,4,5,2],
+ * [1,3,1,5]
+ * ]
+ * Output:
+ * [
+ * [0,0,0,0],
+ * [0,4,5,0],
+ * [0,3,1,0]
+ * ]
+ *
+ * @Author: Zuo Yichen
+ * @Date: 2019/1/26 10:54
+ */
+public class SetMatrixZeroes {
+    public void setZeroes(int[][] matrix) {
+        // this solution is not in-place
+        int row = matrix.length;
+        int column = matrix[0].length;
+        Set<Integer> rowSet = new HashSet<>();
+        Set<Integer> columnSet = new HashSet<>();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                if (matrix[i][j] == 0) {
+                    rowSet.add(i);
+                    columnSet.add(j);
+                }
+            }
+        }
+        for (Integer rowNum : rowSet) {
+            matrix[rowNum] = new int[column];
+        }
+        for (Integer columnNum : columnSet) {
+            for (int i = 0; i < row; i++) {
+                matrix[i][columnNum] = 0;
+            }
+        }
+    }
+
+    public void setZeroes2(int[][] matrix) {
+        boolean fr = false, fc = false;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    if (i == 0) fr = true;
+                    if (j == 0) fc = true;
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (fr) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (fc) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{0, 0, 0, 5}, {4, 3, 1, 4}, {0, 1, 1, 4}, {1, 2, 1, 3}, {0, 0, 1, 1}};
+        new SetMatrixZeroes().setZeroes(matrix);
+    }
+}
+
