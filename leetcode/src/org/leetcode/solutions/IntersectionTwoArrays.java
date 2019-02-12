30fd31fc7a77655041311430a54ba34b19f91c7f
zyc_hp_laptop <271933294@qq.com>
2019-02-12 22:21:42 +0800

    add explanation

--- leetcode/src/org/leetcode/solutions/IntersectionTwoArrays.java
+++ leetcode/src/org/leetcode/solutions/IntersectionTwoArrays.java
@@ -1,11 +1,31 @@
 package org.leetcode.solutions;
 
+/**
+ * Given two arrays, write a function to compute their intersection.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums1 = [1,2,2,1], nums2 = [2,2]
+ * Output: [2]
+ * Example 2:
+ * <p>
+ * Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * Output: [9,4]
+ * Note:
+ * <p>
+ * Each element in the result must be unique.
+ * The result can be in any order.
+ * <p>
+ * * @Author: Zuo Yichen
+ * * @Date: 2019/2/12 22:20
+ */
+
 import java.util.HashSet;
 import java.util.Set;
 
 public class IntersectionTwoArrays {
-	public int[] intersection(int[] nums1, int[] nums2) {
-		Set<Integer> set = new HashSet<>();
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
         Set<Integer> intersect = new HashSet<>();
         for (int i = 0; i < nums1.length; i++) {
             set.add(nums1[i]);
@@ -21,11 +41,11 @@ public class IntersectionTwoArrays {
             result[i++] = num;
         }
         return result;
-	}
+    }
 
-	public static void main(String[] args) {
-		int[] nums1 = { 1, 1, 2, 3 };
-		int[] nums2 = { 2, 2 };
-		System.out.println(new IntersectionTwoArrays().intersection(nums1, nums2));
-	}
+    public static void main(String[] args) {
+        int[] nums1 = {1, 1, 2, 3};
+        int[] nums2 = {2, 2};
+        System.out.println(new IntersectionTwoArrays().intersection(nums1, nums2));
+    }
 }
