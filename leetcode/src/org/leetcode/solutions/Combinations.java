b4590deb4ea0ce60fa5c8329bcbae3b1aca81d97
zyc_hp_laptop <271933294@qq.com>
2019-02-19 23:24:14 +0800

    add combinations

---
+++ leetcode/src/org/leetcode/solutions/Combinations.java
@@ -0,0 +1,28 @@
+package org.leetcode.solutions;
+
+import java.util.List;
+
+/**
+ * Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+ * <p>
+ * Example:
+ * <p>
+ * Input: n = 4, k = 2
+ * Output:
+ * [
+ * [2,4],
+ * [3,4],
+ * [2,3],
+ * [1,2],
+ * [1,3],
+ * [1,4],
+ * ]
+ *
+ * @Author: Zuo Yichen
+ * @Date: 2019/2/19 23:23
+ */
+public class Combinations {
+    public List<List<Integer>> combine(int n, int k) {
+
+    }
+}
