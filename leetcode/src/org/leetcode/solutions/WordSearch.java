927681ac562515d03a1009d907fd0cacb426f5ef
zyc_hp_laptop <271933294@qq.com>
2019-02-24 22:59:31 +0800

    add word search

---
+++ leetcode/src/org/leetcode/solutions/WordSearch.java
@@ -0,0 +1,28 @@
+package org.leetcode.solutions;
+
+/**
+ * Given a 2D board and a word, find if the word exists in the grid.
+ * <p>
+ * The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+ * <p>
+ * Example:
+ * <p>
+ * board =
+ * [
+ * ['A','B','C','E'],
+ * ['S','F','C','S'],
+ * ['A','D','E','E']
+ * ]
+ * <p>
+ * Given word = "ABCCED", return true.
+ * Given word = "SEE", return true.
+ * Given word = "ABCB", return false.
+ *
+ * @Author: Zuo Yichen
+ * @Date: 2019/2/24 16:24
+ */
+public class WordSearch {
+    public boolean exist(char[][] board, String word) {
+
+    }
+}
