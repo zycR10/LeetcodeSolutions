d7ee3487eea575e92ab7ff3109beeb4780acce92
zyc_hp_laptop <271933294@qq.com>
2019-04-06 22:01:20 +0800

    add subset II

---
+++ leetcode/src/org/leetcode/solutions/SubsetsII.java
@@ -0,0 +1,25 @@
+package org.leetcode.solutions;
+
+/**
+ * Given a collection of integers that might contain duplicates, nums, return all possible subsets (the power set).
+ * <p>
+ * Note: The solution set must not contain duplicate subsets.
+ * <p>
+ * Example:
+ * <p>
+ * Input: [1,2,2]
+ * Output:
+ * [
+ * [2],
+ * [1],
+ * [1,2,2],
+ * [2,2],
+ * [1,2],
+ * []
+ * ]
+ *
+ * @Author: Zuo Yichen
+ * @Date: 2019/4/6 22:00
+ */
+public class SubsetsII {
+}
