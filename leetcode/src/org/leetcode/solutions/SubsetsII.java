76bc2bd9e33260a71037c95999010350107329c7
zyc_hp_laptop <271933294@qq.com>
2019-04-07 20:06:55 +0800

    add two solution for SubsetsII

--- leetcode/src/org/leetcode/solutions/SubsetsII.java
+++ leetcode/src/org/leetcode/solutions/SubsetsII.java
@@ -1,5 +1,7 @@
 package org.leetcode.solutions;
 
+import java.util.*;
+
 /**
  * Given a collection of integers that might contain duplicates, nums, return all possible subsets (the power set).
  * <p>
@@ -22,4 +24,54 @@ package org.leetcode.solutions;
  * @Date: 2019/4/6 22:00
  */
 public class SubsetsII {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return res;
+        }
+        // case [4,4,1,4]
+        // [4,1] and [1,4] is same result, should not show twice
+        Arrays.sort(nums);
+//        Set<List<Integer>> set = new HashSet<>();
+        List<Integer> list = new ArrayList<>();
+        search(0, res, nums, list);
+        return res;
+    }
+
+    // 2ms
+    private void search(int index, List<List<Integer>> res,
+                        Set<List<Integer>> set, List<Integer> list,
+                        int[] nums) {
+        if (index > nums.length - 1) {
+            return;
+        }
+        for (int i = index; i < nums.length; i++) {
+            list.add(nums[i]);
+            if (set.add(list)) {
+                res.add(new ArrayList<>(list));
+            }
+            search(i + 1, res, set, list, nums);
+            list.remove(list.size() - 1);
+        }
+    }
+
+    // 1ms, faster
+    private void search (int index, List<List<Integer>> res, int[] nums, List<Integer> list) {
+        res.add(new ArrayList<>(list));
+        for (int i = index; i<nums.length; i++) {
+            // not first time search, so i > index
+            // nums[i-1] == nums[i], it's duplicate
+            if (i > index && nums[i - 1] == nums[i]) {
+                continue;
+            }
+            list.add(nums[i]);
+            search(i + 1, res, nums, list);
+            list.remove(list.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 2};
+        new SubsetsII().subsetsWithDup(nums);
+    }
 }
