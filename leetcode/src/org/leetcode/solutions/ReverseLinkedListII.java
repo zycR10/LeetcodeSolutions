7e9e48d85b711034ad3cdb087d5b67828bdb3aca
zuoyichen@sunlands.com <zuoyichen@sunlands.com>
2019-04-10 20:19:02 +0800

    add Reverse linked list II

---
+++ leetcode/src/org/leetcode/solutions/ReverseLinkedListII.java
@@ -0,0 +1,22 @@
+package org.leetcode.solutions;
+
+import org.leetcode.domain.ListNode;
+
+/**
+ * Reverse a linked list from position m to n. Do it in one-pass.
+ * <p>
+ * Note: 1 ≤ m ≤ n ≤ length of list.
+ * <p>
+ * Example:
+ * <p>
+ * Input: 1->2->3->4->5->NULL, m = 2, n = 4
+ * Output: 1->4->3->2->5->NULL
+ *
+ * @Author: Zuo Yichen
+ * @Date: 2019/4/10 12:14
+ */
+public class ReverseLinkedListII {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+
+    }
+}
