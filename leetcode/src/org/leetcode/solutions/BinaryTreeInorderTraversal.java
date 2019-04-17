545c431b9c97033e1f419b7b35b6ed028aab3f44
zuoyichen@sunlands.com <zuoyichen@sunlands.com>
2019-04-17 20:19:15 +0800

    add problem solution by recursive

--- leetcode/src/org/leetcode/solutions/BinaryTreeInorderTraversal.java
+++ leetcode/src/org/leetcode/solutions/BinaryTreeInorderTraversal.java
@@ -2,14 +2,40 @@ package org.leetcode.solutions;
 
 import org.leetcode.domain.TreeNode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Given a binary tree, return the inorder traversal of its nodes' values.
+ * <p>
+ * Example:
+ * <p>
+ * Input: [1,null,2,3]
+ * 1
+ * \
+ * 2
+ * /
+ * 3
+ * <p>
+ * Output: [1,3,2]
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ *
  * @Author: Zuo Yichen
  * @Date: 2019/4/15 12:32
  */
 public class BinaryTreeInorderTraversal {
     public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorderTraversal(root, res);
+        return res;
+    }
 
+    private void inorderTraversal(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        inorderTraversal(root.left, res);
+        res.add(root.val);
+        inorderTraversal(root.right, res);
     }
 }
