86702eb2c159eae420d5476c354b9a50276d4fe4
zyc_hp_laptop <271933294@qq.com>
2019-05-04 17:18:43 +0800

    use dp to solve problem

--- leetcode/src/org/leetcode/solutions/InterleavingString.java
+++ leetcode/src/org/leetcode/solutions/InterleavingString.java
@@ -1,5 +1,8 @@
 package org.leetcode.solutions;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and s2.
  * <p>
@@ -16,4 +19,32 @@ package org.leetcode.solutions;
  * @Date: 2019/5/3 22:16
  */
 public class InterleavingString {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        char[] c1 = s1.toCharArray(), c2 = s2.toCharArray(), c3 = s3.toCharArray();
+        int m = s1.length(), n = s2.length();
+        if (m + n != s3.length()) {
+            return false;
+        }
+        return dfs(c1, c2, c3, 0, 0, 0, new boolean[m + 1][n + 1]);
+    }
+
+    public boolean dfs(char[] c1, char[] c2, char[] c3, int i, int j, int k, boolean[][] invalid) {
+        if (invalid[i][j]) {
+            return false;
+        }
+        if (k == c3.length) {
+            return true;
+        }
+        boolean valid =
+                i < c1.length && c1[i] == c3[k] && dfs(c1, c2, c3, i + 1, j, k + 1, invalid) ||
+                        j < c2.length && c2[j] == c3[k] && dfs(c1, c2, c3, i, j + 1, k + 1, invalid);
+        if (!valid) {
+            invalid[i][j] = true;
+        }
+        return valid;
+    }
+
+    public static void main(String[] args) {
+        new InterleavingString().isInterleave("aabcc", "dbbca", "aadbbcbcac");
+    }
 }
--- leetcode/src/org/leetcode/solutions/UniqueBinarySearchTreesII.java
+++ leetcode/src/org/leetcode/solutions/UniqueBinarySearchTreesII.java
@@ -32,6 +32,6 @@ import java.util.List;
  */
 public class UniqueBinarySearchTreesII {
     public List<TreeNode> generateTrees(int n) {
-
+        return null;
     }
 }
