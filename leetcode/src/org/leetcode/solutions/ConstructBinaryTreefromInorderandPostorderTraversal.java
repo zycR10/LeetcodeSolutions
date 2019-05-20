f311febabe1ce9f1c183b7d8803401b93fd530b3
zyc_hp_laptop <271933294@qq.com>
2019-05-20 23:09:16 +0800

    add construct binary tree from inorder and postorder traversal

---
+++ leetcode/src/org/leetcode/solutions/ConstructBinaryTreefromInorderandPostorderTraversal.java
@@ -0,0 +1,30 @@
+package org.leetcode.solutions;
+
+import org.leetcode.domain.TreeNode;
+
+/**
+ * Given inorder and postorder traversal of a tree, construct the binary tree.
+ * <p>
+ * Note:
+ * You may assume that duplicates do not exist in the tree.
+ * <p>
+ * For example, given
+ * <p>
+ * inorder = [9,3,15,20,7]
+ * postorder = [9,15,7,20,3]
+ * Return the following binary tree:
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ *
+ * @Author: Zuo Yichen
+ * @Date: 2019/5/20 22:58
+ */
+public class ConstructBinaryTreefromInorderandPostorderTraversal {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+
+    }
+}
