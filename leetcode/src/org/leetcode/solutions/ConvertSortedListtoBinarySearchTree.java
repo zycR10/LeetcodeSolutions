32d7af4d15f31acf2bcb46d8afe4a7927c2f1c80
zyc_hp_laptop <271933294@qq.com>
2019-05-26 22:25:55 +0800

    add convert sorted list to binary search tree

---
+++ leetcode/src/org/leetcode/solutions/ConvertSortedListtoBinarySearchTree.java
@@ -0,0 +1,14 @@
+package org.leetcode.solutions;
+
+import org.leetcode.domain.ListNode;
+import org.leetcode.domain.TreeNode;
+
+/**
+ * @Author: Zuo Yichen
+ * @Date: 2019/5/26 18:32
+ */
+public class ConvertSortedListtoBinarySearchTree {
+    public TreeNode sortedListToBST(ListNode head) {
+
+    }
+}
