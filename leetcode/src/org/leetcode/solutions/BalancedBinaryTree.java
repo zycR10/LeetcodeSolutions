9063f2f8d99305753284d7c723b5444e5b957100
zyc_hp_laptop <271933294@qq.com>
2019-06-11 23:27:34 +0800

    review BalancedBinaryTree

--- leetcode/src/org/leetcode/solutions/BalancedBinaryTree.java
+++ leetcode/src/org/leetcode/solutions/BalancedBinaryTree.java
@@ -2,29 +2,63 @@ package org.leetcode.solutions;
 
 import org.leetcode.domain.TreeNode;
 
-public class BalancedBinaryTree {
-	public boolean isBalanced(TreeNode root) {
-		if (root == null) {
-			return true;
-		}
-		return height(root) != -1;
-	}
+/**
+ * Given a binary tree, determine if it is height-balanced.
+ * <p>
+ * For this problem, a height-balanced binary tree is defined as:
+ * <p>
+ * a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+ * <p>
+ * Example 1:
+ * <p>
+ * Given the following tree [3,9,20,null,null,15,7]:
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * Return true.
+ * <p>
+ * Example 2:
+ * <p>
+ * Given the following tree [1,2,2,3,3,null,null,4,4]:
+ * <p>
+ * 1
+ * / \
+ * 2   2
+ * / \
+ * 3   3
+ * / \
+ * 4   4
+ * Return false.
+ */
 
-	public int height(TreeNode node) {
-		if (node == null) {
-			return 0;
-		}
-		int lH = height(node.left);
-		if (lH == -1) {
-			return -1;
-		}
-		int rH = height(node.right);
-		if (rH == -1) {
-			return -1;
-		}
-		if (lH - rH < -1 || lH - rH > 1) {
-			return -1;
-		}
-		return Math.max(lH, rH) + 1;
-	}
+public class BalancedBinaryTree {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return height(root) != -1;
+    }
+
+    public int height(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int lH = height(node.left);
+        // if LH or RH = -1 means |LH - RH| > 1
+        // so we should stop the recursion early
+        if (lH == -1) {
+            return -1;
+        }
+        int rH = height(node.right);
+        if (rH == -1) {
+            return -1;
+        }
+        if (lH - rH < -1 || lH - rH > 1) {
+            return -1;
+        }
+        return Math.max(lH, rH) + 1;
+    }
 }
