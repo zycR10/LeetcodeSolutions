8b07dcfe6e58baa884af3eba4edc38966864dc11
zyc_hp_laptop <271933294@qq.com>
2019-08-04 19:40:34 +0800

    ok, i'm idiot

--- leetcode/src/org/leetcode/solutions/SumRootToLeafNumbers.java
+++ leetcode/src/org/leetcode/solutions/SumRootToLeafNumbers.java
@@ -2,6 +2,11 @@ package org.leetcode.solutions;
 
 import org.leetcode.domain.TreeNode;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
  * <p>
@@ -42,6 +47,17 @@ import org.leetcode.domain.TreeNode;
  */
 public class SumRootToLeafNumbers {
     public int sumNumbers(TreeNode root) {
+        return sum(root, 0);
+    }
 
+    public int sum(TreeNode n, int s) {
+        if (n == null) {
+            return 0;
+        }
+        if (n.right == null && n.left == null) {
+            return s * 10 + n.val;
+        }
+        return sum(n.left, s * 10 + n.val) + sum(n.right, s * 10 + n.val);
     }
 }
+
