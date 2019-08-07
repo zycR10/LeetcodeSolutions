b7b484460b67baa8e3c8b9d141aa4eddccc599bc
zuoyichen@sunlands.com <zuoyichen@sunlands.com>
2019-08-07 23:23:50 +0800

    add palindrome partitioning

---
+++ leetcode/src/org/leetcode/solutions/PalindromePartitioning.java
@@ -0,0 +1,26 @@
+package org.leetcode.solutions;
+
+import java.util.List;
+
+/**
+ * Given a string s, partition s such that every substring of the partition is a palindrome.
+ * <p>
+ * Return all possible palindrome partitioning of s.
+ * <p>
+ * Example:
+ * <p>
+ * Input: "aab"
+ * Output:
+ * [
+ * ["aa","b"],
+ * ["a","a","b"]
+ * ]
+ *
+ * @Author: Zuo Yichen
+ * @Date: 2019/8/7 12:24
+ */
+public class PalindromePartitioning {
+    public List<List<String>> partition(String s) {
+
+    }
+}
