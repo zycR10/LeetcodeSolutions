ce97d73f87c53e5d9436e4882d14376abb9c6f3f
zyc_hp_laptop <271933294@qq.com>
2019-11-10 23:35:00 +0800

    add Triangle

---
+++ src/main/java/solutions/Triangle.java
@@ -0,0 +1,25 @@
+package solutions;
+
+import java.util.List;
+
+/**
+ * Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
+ * <p>
+ * For example, given the following triangle
+ * <p>
+ * [
+ * [2],
+ * [3,4],
+ * [6,5,7],
+ * [4,1,8,3]
+ * ]
+ * The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+ *
+ * @Author: Zuo Yichen
+ * @Date: 2019/11/10 23:34
+ */
+public class Triangle {
+    public int minimumTotal(List<List<Integer>> triangle) {
+
+    }
+}
