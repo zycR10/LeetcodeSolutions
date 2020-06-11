2867cd4498b14ccfae0faf3c90c4b861692d4931
zyc-pro <271933294@qq.com>
2020-06-11 23:32:47 +0800

    complete FileGenerator.java

--- src/main/java/util/FileGenerator.java
+++ src/main/java/util/FileGenerator.java
@@ -1,8 +1,10 @@
 package util;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileGenerator {
     public static void main(String[] args) {
@@ -15,14 +17,15 @@ public class FileGenerator {
     private void createFile(String[] args) {
         ClassLoader cl = getClass().getClassLoader();
         for (String name : args) {
-            String fileName = name + ".md";
+            String fileName = name.replaceAll(" ", "") + ".md";
             URL url = cl.getResource(fileName);
 
             if (url == null) {
-                File newFile = new File("\\resource\\" + fileName);
-                System.out.println(this.getClass().getClassLoader().getResource("").getPath());
                 try {
-                    newFile.createNewFile();
+                    Path path = Paths.get("D:\\github\\LeetcodeSolutions\\src\\main\\resource\\" + fileName);
+                    StringBuilder sb = new StringBuilder();
+                    sb.append("# " + name + "\r\n").append("## 题目 \r\n").append("## 思路 \r\n").append("## 实现 \r\n");
+                    Files.write(path, sb.toString().getBytes());
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
